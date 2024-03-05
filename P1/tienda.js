e5082846e4636ab3a0a6c8e61decdeb066d3965c
CarlosCruceta <c.cruceta.2016@alumnos.urjc.es>
2024-03-05 02:04:49 +0100

    recurso ls añadido

--- P1/tienda.js
+++ P1/tienda.js
@@ -1,39 +1,43 @@
-
-//-- Servidor que devuelve una página en HTML con mi tienda cuando se le pide
-//-- el recurso raiz (/), y devuelve una página de ERROR también
-//-- en HTML cuando se pide cualquier otro recurso
-
-// Importacion de módulos
 const fs = require('fs');
 const http = require('http');
 
-// Puerto donde escucha el servidor
 const port = 9090;
 
-// Creacion del servidor
 const server = http.createServer((req, res) => {
-    // Analizar la URL solicitada
     const url = new URL(req.url, 'http://' + req.headers['host']);
     let file = '';
     let code = 200;
-
-    // Determinar el archivo solicitado
-    if (url.pathname == '/') {
-        file = 'index.html';
-    } else {
-        file = url.pathname.substr(1);
-    }
-
-// Obtener el formato del archivo a partir de su extensión
-    let fileFormat = file.split('.')[1];
-
-    // Establecer el tipo MIME predeterminado
     let fileType = 'text/html';
 
-    // Configurar el mensaje de estado de respuesta
-    res.statusMessage = "OK";
+    if (url.pathname === '/') {
+        file = 'index.html';
+        
+    } else if (url.pathname === '/ls') {
+        // Manejo del recurso /ls para obtener el listado de archivos
+        fs.readdir('.', (err, files) => {
+            if (err) {
+                console.error(`Error al leer el directorio: ${err.message}`);
+                res.statusCode = 500;
+                res.statusMessage = "INTERNAL SERVER ERROR";
+                res.setHeader('Content-Type', fileType);
+                return res.end('Error en la carga de la página');
+            }
+            // Creamos el html con el listado de archivos
+            const fileListHTML = `<html><body><h1>Lista de Archivos:</h1><ul>${files.map(file => `<li>${file}</li>`).join('')}</ul></body></html>`;
+            res.statusCode = code;
+            res.setHeader('Content-Type', fileType);
+            res.write(fileListHTML);
+            return res.end();
+        });
+        return;
+    } else {
+        file = url.pathname.substr(1);
+        
+    }
+  
+    let fileFormat = file.split('.')[1];
+    
 
-    // Determinar el tipo MIME específico para ciertos formatos de archivo
     switch (fileFormat) {
         case 'css':
         case 'html':
@@ -49,32 +53,25 @@ const server = http.createServer((req, res) => {
             fileType = 'application/' + fileFormat;
             break;
         default:
-            // Si no coincide con ningún formato conocido, configurar el código de respuesta 404
             code = 404;
             res.statusMessage = "NOT FOUND";
             file = 'error.html';
     }
 
-    // Leer el archivo y responder al cliente
     fs.readFile(file, (err, data) => {
         if (err) {
-            // Si hay un error al leer el archivo, configurar el código de respuesta 404
             res.statusCode = 404;
             res.statusMessage = "NOT FOUND";
             res.setHeader('Content-Type', fileType);
             return res.end('Error en la carga de la página');
         }
 
-        // Configurar el código de respuesta y el tipo MIME, y enviar el contenido del archivo
         res.statusCode = code;
         res.setHeader('Content-Type', fileType);
         res.write(data);
         return res.end();
     });
-
-
 });
-
 
 server.listen(port);
 console.log("Escuchando en puerto: " + port);
