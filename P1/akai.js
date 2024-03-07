8c4206d053e34b277e1f93412b0796ffd3257963
CarlosCruceta <c.cruceta.2016@alumnos.urjc.es>
2024-03-07 03:11:37 +0100

    plantilla de productos

--- P1/akai.js
+++ P1/akai.js
@@ -5,10 +5,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Textos predefinidos asociados a cada punto
     var paragraphs = [
-        "<b>OPTIMIZACIÓN DEL LIVE PLAYBACK<b><br><br>Botón de repetición de nota más grande y visible, junto con la Banda Inteligente, brindan nuevas posibilidades para tocar redobles, arpegios y mucho más.",
-        "<b>PANTALLAS DE ALTA RESOLUCIÓN<b><br><br>Dos pantallas a color de alta resolución con encoder pulsable ofrecen una forma visual e intuitiva de buscar sonidos, efectos, plug-ins e instrumentos con una sola mano.",
-        "<b>INTERFAZ PROFESIONAL DE AUDIO<b><br><br>Disfruta de un sonido brillante con la interfaz profesional de audio a 96 kHz y 24 bits, garantizando una calidad excepcional en tu producción musical.",
-        "<b>PADS MEJORADOS<b><br><br>Disfruta de una experiencia más fluida y expresiva gracias a los pads mejorados, que son ahora más amplios y sensibles. Esto te proporciona la capacidad de tocar con una precisión y dinamismo excepcionales.",
+        "<b>OPTIMIZACIÓN DEL LIVE PLAYBACK</b><br><br>Botón de repetición de nota más grande y visible, junto con la Banda Inteligente, brindan nuevas posibilidades para tocar redobles, arpegios y mucho más.",
+        "<b>PANTALLAS DE ALTA RESOLUCIÓN</b><br><br>Dos pantallas a color de alta resolución con encoder pulsable ofrecen una forma visual e intuitiva de buscar sonidos, efectos, plug-ins e instrumentos con una sola mano.",
+        "<b>INTERFAZ PROFESIONAL DE AUDIO</b><br><br>Disfruta de un sonido brillante con la interfaz profesional de audio a 96 kHz y 24 bits, garantizando una calidad excepcional en tu producción musical.",
+        "<b>PADS MEJORADOS</b><br><br>Disfruta de una experiencia más fluida y expresiva gracias a los pads mejorados, que son ahora más amplios y sensibles. Esto te proporciona la capacidad de tocar con una precisión y dinamismo excepcionales.",
     ];
 
     // Por defecto, seleccionar el primer punto e imagen
