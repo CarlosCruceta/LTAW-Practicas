f51369c81dfa1c059b733c52993ddf9022cbbf39
CarlosCruceta <c.cruceta.2016@alumnos.urjc.es>
2024-03-07 15:00:24 +0100

    segundo producto añadido

--- P1/maschine.js
+++ P1/maschine.js
@@ -5,10 +5,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Textos predefinidos asociados a cada punto
     var paragraphs = [
-        "<b>16 POTENCIÓMETROS Q-LINK CON PANTALLAS OLED</b><br><br>Enlaza tus efecto favoritos de forma personalizada, observando todos los parámetros necesarios nítidamente gracias a sus fantásticas pantallas OLED.",
-        "<b>PANTALLA TÁCTIL MULTI-TOUCH A COLOR</b><br><br>Pantalla táctil multigestual a todo color de 10,1 pulgadas con ángulo ajustable.",
-        "<b>TODAS LAS I/O QUE NECESITES</b><br>2 entradas combo XLR/jack con alimentación phantom, 2 entradas jack TRS/RCA línea/phono, 8 salidas TRS 6,35 mm, 8 salidas TS CV/Gate, 2 E/S MIDI 5 pines, Salidas de auriculares jack y minijak",
-        "<b>PADS MPC ILUMINADOS</b><br><br>16 velocity- and pressure-sensitive pads, retroiluminados RGB que permiten un tacto cómodo y firme, controlando cada sonido de manera única.",
+        "<b>OPTIMIZACIÓN DEL LIVE PLAYBACK</b><br><br>Botón de repetición de nota más grande y visible, junto con la Banda Inteligente, brindan nuevas posibilidades para tocar redobles, arpegios y mucho más.",
+        "<b>PANTALLAS DE ALTA RESOLUCIÓN</b><br><br>Dos pantallas a color de alta resolución con encoder pulsable ofrecen una forma visual e intuitiva de buscar sonidos, efectos, plug-ins e instrumentos con una sola mano.",
+        "<b>INTERFAZ PROFESIONAL DE AUDIO</b><br><br>Disfruta de un sonido brillante con la interfaz profesional de audio a 96 kHz y 24 bits, garantizando una calidad excepcional en tu producción musical.",
+        "<b>PADS MEJORADOS</b><br><br>Disfruta de una experiencia más fluida y expresiva gracias a los pads mejorados, que son ahora más amplios y sensibles. Esto te proporciona la capacidad de tocar con una precisión y dinamismo excepcionales.",
     ];
 
     // Por defecto, seleccionar el primer punto e imagen
