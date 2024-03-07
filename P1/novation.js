e46e303ae57d687f75965cb8e6cb873584e6a63d
CarlosCruceta <c.cruceta.2016@alumnos.urjc.es>
2024-03-07 23:30:08 +0100

    paginas de productos listas

--- P1/novation.js
+++ P1/novation.js
@@ -5,10 +5,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Textos predefinidos asociados a cada punto
     var paragraphs = [
-        "<b>16 POTENCIÓMETROS Q-LINK CON PANTALLAS OLED</b><br><br>Enlaza tus efecto favoritos de forma personalizada, observando todos los parámetros necesarios nítidamente gracias a sus fantásticas pantallas OLED.",
-        "<b>PANTALLA TÁCTIL MULTI-TOUCH A COLOR</b><br><br>Pantalla táctil multigestual a todo color de 10,1 pulgadas con ángulo ajustable.",
-        "<b>TODAS LAS I/O QUE NECESITES</b><br>2 entradas combo XLR/jack con alimentación phantom, 2 entradas jack TRS/RCA línea/phono, 8 salidas TRS 6,35 mm, 8 salidas TS CV/Gate, 2 E/S MIDI 5 pines, Salidas de auriculares jack y minijak",
-        "<b>PADS MPC ILUMINADOS</b><br><br>16 velocity- and pressure-sensitive pads, retroiluminados RGB que permiten un tacto cómodo y firme, controlando cada sonido de manera única.",
+        "<b>PADS Y BOTONES INTUITIVOS</b><br><br>32 pads retroiluminados. Los pads sensibles a la velocidad ofrecen una experiencia táctil excepcional para la creación de ritmos, mientras que los botones asignables te permiten controlar de manera fácil y rápida funciones esenciales durante tu sesión de producción.",
+        "<b>EDICIÓN LIMITADA 'GREY VERSION'</b><br><br>Descubre la elegancia en la edición limitada del Novation Circuits Tracks.  Haz una declaración de estilo mientras desatas tu creatividad musical explotando todas sus capacidades de producción.",
+        "<b>CONEXIONES VERSÁTILES</b><br><br>Entradas y salidas MIDI y conexiones USB. Una conectividad digital avanzada que permite integrar el Circuits Tracks en tu estudio o actuación. Aprovecha al máximo la sincronización de reloj y la integración con software de producción.",
+        "<b>POTENCIÓMETROS Y EFECTOS DINÁMICOS</b><br><br>Ajusta con precisión tu sonido con los 8 potenciómetros táctiles y asignables. Ofrece una variedad de efectos integrados, desde filtros y reverberaciones hasta distorsiones. La versatilidad de los potenciómetros te brinda un control total sobre tu mezcla."
     ];
 
     // Por defecto, seleccionar el primer punto e imagen
