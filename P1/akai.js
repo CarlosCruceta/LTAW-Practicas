636d414a6dca74d201ca94e9fb596faa8760e393
CarlosCruceta <c.cruceta.2016@alumnos.urjc.es>
2024-03-07 02:23:25 +0100

    css avanzado

--- P1/akai.js
+++ P1/akai.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Textos predefinidos asociados a cada punto
     var paragraphs = [
-        "<b>OPTIMIZACIÓN DEl LIVE PLAYBACK<b><br><br>Botón de repetición de nota más grande y visible, junto con la Banda Inteligente, brindan nuevas posibilidades para tocar redobles, arpegios y mucho más.",
+        "<b>OPTIMIZACIÓN DEL LIVE PLAYBACK<b><br><br>Botón de repetición de nota más grande y visible, junto con la Banda Inteligente, brindan nuevas posibilidades para tocar redobles, arpegios y mucho más.",
         "<b>PANTALLAS DE ALTA RESOLUCIÓN<b><br><br>Dos pantallas a color de alta resolución con encoder pulsable ofrecen una forma visual e intuitiva de buscar sonidos, efectos, plug-ins e instrumentos con una sola mano.",
         "<b>INTERFAZ PROFESIONAL DE AUDIO<b><br><br>Disfruta de un sonido brillante con la interfaz profesional de audio a 96 kHz y 24 bits, garantizando una calidad excepcional en tu producción musical.",
         "<b>PADS MEJORADOS<b><br><br>Disfruta de una experiencia más fluida y expresiva gracias a los pads mejorados, que son ahora más amplios y sensibles. Esto te proporciona la capacidad de tocar con una precisión y dinamismo excepcionales.",
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 imageText.style.display = 'block';
             } else {
                 // Aplicar una opacidad menor para las otras imágenes
-                image.style.opacity = '0.3';
+                image.style.opacity = '0.2';
             }
         });
 
